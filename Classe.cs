0435897441213070f28c97c67f73b21fabc07bcd
PaulW9 <weimer.paul@orange.fr>
2024-11-25 12:18:06 +0100

    Ajoutez des fichiers projet.

---
+++ Classe.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Classe
+    {
+        public string nomClasse;
+        public List<Eleve> eleves;
+        public List<String> matieres;
+        public readonly int NbElevesMax = 30;
+        public readonly int NbMatieresMax = 10;
+
+
+        public Classe(string nomClasse)
+        {
+            this.eleves = new List<Eleve>();
+            this.matieres = new List<String>();
+            this.nomClasse = nomClasse;
+        }
+
+        internal void ajouterEleve(string prenom, string nom)
+        {
+            try
+            {
+                eleves.Add(new Eleve(prenom, nom));
+
+                if (NbElevesMax <= eleves.Count)
+                {
+                    Console.WriteLine($"Le nombre d'élève par classe ne peut pas dépasser {NbElevesMax}.");
+                }
+
+                if (eleves.Count <= 0)
+                {
+                    Console.WriteLine("Le nombre d'élève par classe ne peut pas être nul ou négatif.");
+                }
+
+            }
+            catch
+            {
+                Console.WriteLine("Veuillez saisir un nombre d'élève valide.");
+            }
+        }
+
+        internal void ajouterMatiere(string nomMatiere)
+        {
+            try
+            {
+                matieres.Add(nomMatiere);
+
+                if (NbMatieresMax <= matieres.Count)
+                {
+                    Console.WriteLine($"Le nombre de matière par classe ne doit pas dépasser {NbMatieresMax}.");
+                }
+
+                if (matieres.Count <= 0)
+                {
+                    Console.WriteLine("Le nombre de matière ne peut pas être négatif ou nul.");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Veuillez saisir un nombre de matière valide.");
+            }
+        }
+
+        internal float moyenneGeneral()
+        {
+            float SommeMoyenne = 0;
+            for(int i = 0; i < matieres.Count; i += 1)
+            {
+                SommeMoyenne += moyenneMatiere(i);
+            }
+            return (float)(Math.Truncate(100 * (SommeMoyenne / matieres.Count)) / 100);
+        }
+
+        internal float moyenneMatiere(int LaMatiere)
+        {
+            float SommeMoyenne = 0;
+            for (int i = 0; i < eleves.Count; i += 1)
+            {
+                SommeMoyenne += eleves[i].MoyEleveMatiere( LaMatiere);
+            }
+            return (float)(Math.Truncate(100 * (SommeMoyenne/eleves.Count)) / 100);
+        }
+    }
+}
---
+++ Eleve.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HNI_TPmoyennes
+{
+    internal class Eleve
+    {
+        public string prenom;
+        public string nom;
+        public List<Note> notes;
+        public readonly int nbNotesmax = 200;
+
+        public Eleve(string prenom, string nom)
+        {
+            this.prenom = prenom;
+            this.nom = nom;
+            this.notes = new List<Note>();
+        }
+
+        internal void ajouterNote(Note note)
+        {
+            try
+            {
+                notes.Add(note);
+                {
+                    if (nbNotesmax <= notes.Count)
+                    {
+                        Console.WriteLine($"Le nombre de note d'un élève ne peut pas être supérieur à {nbNotesmax}.");
+                    }
+
+                    if (notes.Count <= 0)
+                    {
+                        Console.WriteLine("Le nombre de note d'un élève ne peut pas être nul ou négatif.");
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Veuillez saisir un nombre de note valide.");
+            }
+        }
+
+        internal float moyenneGeneral()
+        {
+            int CptNotes = 0;
+            float SommeNotes = 0;
+            float MoyEleveMatiere;
+            for (int i = 0; i < 10; i += 1)
+            {
+                try
+                {
+                    MoyEleveMatiere = this.MoyEleveMatiere(i);
+
+                    if (MoyEleveMatiere != 0 && MoyEleveMatiere >= 3)
+                    {
+                        SommeNotes += MoyEleveMatiere;
+                        CptNotes += 1;
+                    }      
+                }
+                catch
+                {
+                    Console.WriteLine("Veuillez saisir une moyenne correcte pour l'élève en question.");
+                }
+            }
+            return (float)(Math.Truncate(100 * (SommeNotes / CptNotes)) / 100);
+        }
+
+        internal float MoyEleveMatiere(int LaMatiere)
+        {
+            float SommeNotes = 0;
+            int CptNotes = 0;
+            for (int i = 0; i < notes.Count; i += 1)
+            {
+                if (notes[i].matiere == LaMatiere)
+                {
+                    SommeNotes += notes[i].note;
+                    CptNotes += 1;
+                }
+            }
+            if (CptNotes == 0)
+                return 0;
+            else 
+                return (float)(Math.Truncate(100 * (SommeNotes/CptNotes)) / 100);
+        }
+    }
+}
